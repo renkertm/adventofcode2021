10de9ed323647a1fc7998a613a30184439240264
renkertm <58270935+renkertm@users.noreply.github.com>
2021-12-02 19:33:12 +0100

    Day-02

---
+++ Day-02/index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+var input = fs.readFileSync("input.txt");
+var instruction = [];
+var pos = 0;
+var depth = 0;
+var aim = 0;
+var lines = input.toString().split('\n');
+lines.forEach(element => {
+    instruction.push(element.split(' '))
+});
+instruction.forEach(element => {
+    switch(element[0]){
+        case("forward"):
+            pos += parseInt(element[1]);
+            depth += parseInt(element[1]) * aim;
+            break;
+
+        case("down"):
+            aim += parseInt(element[1]);
+            break;
+        
+        case("up"):
+            aim -= parseInt(element[1]);
+            break;
+
+        default:
+            break;
+    }
+});
+console.log(pos * depth);
