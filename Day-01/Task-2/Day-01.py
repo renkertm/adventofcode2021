8029c7d3573a2ffb8e995698adf96c87fd60816b
renkertm <58270935+renkertm@users.noreply.github.com>
2021-12-03 23:25:49 +0100

    Day-01

--- Day-01/Task-2/Day-01.py
+++ Day-01/Task-2/Day-01.py
@@ -7,7 +7,6 @@ last = 0
 list = []
 for i in range(len(inputlist) - 2):
     list.append(int(inputlist[i]) + int(inputlist[i+1]) + int(inputlist[i+2]))
-print(len(list))
 for i in list:
     if last != 0 and int(i) > last and i != last:
         increasments += 1
